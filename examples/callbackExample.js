d3e104fd2e5d39797176b4229fc7abd01eeb9925
John Azzaro <jazzaro@temple.edu>
2019-08-18 10:51:51 -0700

    modfications

--- examples/callbackExample.js
+++ examples/callbackExample.js
@@ -1,16 +1,14 @@
-
-console.log('Before');
+console.log("Before");
 const user = getUser(1);
 console.log(user);
-console.log('After');
+console.log("After");
 
-function getUser(id) {                               
-    setTimeout(function() {
-            console.log('Reading a user from a database...');
-            return { id: id, Username: 'Alan' }   ;                       
-    }, 2000);
+function getUser(id) {
+   setTimeout(function() {
+      console.log("Reading a user from a database...");
+      return { id: id, Username: "Alan" };
+   }, 2000);
 }
-
 
 /* 
 The result will be:
